79ff7508870bfe0531cfca55955d25b497a1220f
lucasjcfreire <lucasjcfreire@outlook.com>
2025-05-09 22:08:38 -0300

    feat(ui): add shadcn/ui components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button';
 import type { ReactElement } from 'react';
 
 export function Home(): ReactElement {
 	return (
 		<div>
 			<h1 className="text-2xl text-red-600">Home</h1>
+			<Button>Button</Button>
 		</div>
 	);
 }
