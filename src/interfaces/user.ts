1afca36b1f6fe501deecb25175511e34c44853b3
lucasjcfreire <lucasjcfreire@outlook.com>
2025-05-11 18:44:37 -0300

    feat(users-mock): add user and data mocks for testing and UI population

---
+++ src/interfaces/user.ts
@@ -0,0 +1,16 @@
+export interface CardItem {
+	title: string;
+	value: string | number;
+}
+
+export interface User {
+	id: number;
+	initials: string;
+	name: string;
+	age: number;
+	gender: string;
+	date: string;
+	time: string;
+	duration: string;
+	status: string;
+}
