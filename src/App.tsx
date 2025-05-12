3323ab379a5aec8539fd9903e0d423634ba11ced
lucasjcfreire <lucasjcfreire@outlook.com>
2025-05-12 09:27:43 -0300

    feat(layout): implement AdminLayout with sidebar and navigation components

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
-import { Layout } from '@/components/layout';
+import { AdminLayout } from '@/components/layout/AdminLayout';
 import { Dashboard, Documents, NotFound, Settings, Users } from '@/pages';
 import type { ReactElement } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { AppSidebar } from './components/layout/sidebar/AppSidebar';
-import { SidebarProvider } from './components/ui/sidebar';
 
 export default function App(): ReactElement {
 	return (
 		<BrowserRouter>
-			<SidebarProvider
-				style={{ '--sidebar-width': '240px' } as React.CSSProperties}
-			>
-				<AppSidebar />
-				<Routes>
-					<Route path="/" element={<Navigate to="/fa/usuarios" replace />} />
-					<Route path="/:filial" element={<Layout />}>
-						<Route path="dashboard" element={<Dashboard />} />
-						<Route path="usuarios" element={<Users />} />
-						<Route path="documentos" element={<Documents />} />
-					</Route>
+			<Routes>
+				<Route path="/" element={<Navigate to="/fa/usuarios" replace />} />
+				<Route path="/:filial" element={<AdminLayout />}>
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="usuarios" element={<Users />} />
+					<Route path="documentos" element={<Documents />} />
+				</Route>
 
-					<Route path="/" element={<Layout />}>
-						<Route path="configuracoes" element={<Settings />} />
-					</Route>
+				<Route path="/" element={<AdminLayout />}>
+					<Route path="configuracoes" element={<Settings />} />
+				</Route>
 
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</SidebarProvider>
+				<Route path="*" element={<NotFound />} />
+			</Routes>
 		</BrowserRouter>
 	);
 }
